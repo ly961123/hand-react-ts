8bdeace507f48d01d2e9cebe2223062dea902bb9
v_yonlai <v_yonlai@tencent.com>
2020-09-14 17:38:07 +0800

    删除logger多余demo

--- portal/logger/index.ts
+++ portal/logger/index.ts
@@ -2,28 +2,17 @@ import ip from 'ip';
 import moment from 'moment';
 import { createLogger, format, transports } from 'winston';
 
-interface ILoggerConfig {
-  isClientLogger?: boolean;
-}
-
-type TLoggerConfig = ILoggerConfig | undefined;
-
 const baseLogPath = process.env.LOG_PATH || 'logs';
 const getLoggerFileName = (name: string) => {
   const ipAddress = ip.address();
   return `${baseLogPath}/${name}.log.${moment().format('YYYY-MM-DD')}.${ipAddress}`;
 };
 
-const logger = (config: TLoggerConfig = {}) => {
+const logger = () => {
   const { combine, timestamp, label, printf } = format;
   const logFormat = printf(({ level, message, label: formatLabel }) => `${moment().format('YYYY-MM-DD HH:mm:ss')} [${formatLabel}] ${level}: ${message}`);
 
-  const transportList = config.isClientLogger ? [
-    new transports.Console(),
-    new transports.File({
-      filename: getLoggerFileName('client-error'), level: 'error',
-    }),
-  ] : [
+  const transportList = [
     new transports.Console(),
     new transports.File({
       filename: getLoggerFileName('error'), level: 'error',
