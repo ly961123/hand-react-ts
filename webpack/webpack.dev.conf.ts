bb4b603ce78aea51a9a1b5eb5d5a6282f43f36ea
v_yonlai <v_yonlai@tencent.com>
2020-11-10 17:43:07 +0800

    webpack增加eslint规范

--- webpack/webpack.dev.conf.ts
+++ webpack/webpack.dev.conf.ts
@@ -131,12 +131,12 @@ const config = {
   },
   optimization: {
     splitChunks: {
-      chunks: "all",   // 共有3个值"initial"，"async"和"all"。配置后，代码分割优化仅选择初始块，按需块或所有块
-			minSize: 30000,   // （默认值：30000）块的最小大小
-			minChunks: 2,    // （默认值：1）在拆分之前共享模块的最小块数
-			maxAsyncRequests: 5,   //（默认为5）按需加载时并行请求的最大数量
-			maxInitialRequests: 3,  //（默认值为3）入口点的最大并行请求数
-			automaticNameDelimiter: '.',  // 默认情况下，webpack将使用块的来源和名称生成名称，例如vendors~main.js
+      chunks: 'all', // 共有3个值"initial"，"async"和"all"。配置后，代码分割优化仅选择初始块，按需块或所有块
+      minSize: 30000, // （默认值：30000）块的最小大小
+      minChunks: 2, // （默认值：1）在拆分之前共享模块的最小块数
+      maxAsyncRequests: 5, //（默认为5）按需加载时并行请求的最大数量
+      maxInitialRequests: 3, //（默认值为3）入口点的最大并行请求数
+      automaticNameDelimiter: '.', // 默认情况下，webpack将使用块的来源和名称生成名称，例如vendors~main.js
     },
   },
 };
